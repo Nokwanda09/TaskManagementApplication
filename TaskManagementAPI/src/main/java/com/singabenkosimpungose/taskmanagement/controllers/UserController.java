f777050adb76380f3008225ca2c82e995231ac73
Singabenkosi Mpungose <mpungosesingabenkosi@gmail.com>
2025-02-19 16:11:27 +0200

    Login page works

--- TaskManagementAPI/src/main/java/com/singabenkosimpungose/taskmanagement/controllers/UserController.java
+++ TaskManagementAPI/src/main/java/com/singabenkosimpungose/taskmanagement/controllers/UserController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("user")
-@CrossOrigin(origins = "http://127.0.0.1:5050")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class UserController {
 
     @Autowired
--- Web/src/javascript/login.js
+++ Web/src/javascript/login.js
@@ -1,5 +1,5 @@
 const loginForm = document.getElementById("login-form");
-
+const cardBody = document.getElementById("card-body");
 
 function loginUser(event) {
   event.preventDefault();
@@ -19,10 +19,36 @@ function loginUser(event) {
       if (data.exists) {
         window.location.href = "/Web/HTML/displayTasks.html";
       } else {
-        window.location.href = "/Web/HTML/createProfile.html";
+        // window.location.href = "/Web/HTML/createProfile.html";
+        changeCardContent();
+        addCreateProfileBtn();
       }
     });
 }
 
+function changeCardContent() {
+  loginForm.style.display = "none";
+  const promptParagraph = document.createElement("p");
+  promptParagraph.innerText =
+    "You currently do not have a profile.\nPlease click the button to create Account.";
+  // console.log(cardBody);
+  cardBody.appendChild(promptParagraph);
+  promptParagraph.style.display = "block";
+  promptParagraph.style.textAlign = "center";
+}
+
+function addCreateProfileBtn() {
+  const createProfileBtn = document.createElement("div");
+  createProfileBtn.classList.add("enter-btn");
+  createProfileBtn.innerHTML = ` <button type="submit">Create Profile</button>`;
+
+  cardBody.appendChild(createProfileBtn);
+  createProfileBtn.style.display = "flex";
+  createProfileBtn.style.justifyContent = "center";
+
+  createProfileBtn.addEventListener("click", () => {
+    window.location.href = "/Web/HTML/createProfile.html";
+  });
+}
 
 loginForm.addEventListener("submit", loginUser);

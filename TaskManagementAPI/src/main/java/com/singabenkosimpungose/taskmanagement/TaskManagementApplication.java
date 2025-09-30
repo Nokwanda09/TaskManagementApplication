38816d16e824e83b97b620a6f709db7d08c5d1f2
Singabenkosi Mpungose <mpungosesingabenkosi@gmail.com>
2025-09-30 11:23:56 +0200

    Fixed the errors, program run

--- TaskManagementAPI/src/main/java/com/singabenkosimpungose/taskmanagement/TaskManagementApplication.java
+++ TaskManagementAPI/src/main/java/com/singabenkosimpungose/taskmanagement/TaskManagementApplication.java
@@ -3,6 +3,8 @@ package com.singabenkosimpungose.taskmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class TaskManagementApplication { 
 
